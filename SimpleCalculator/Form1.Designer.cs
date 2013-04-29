5a5903b9ed0fa2f5d4e1745df16de4092d5778b5
Seven Menta <7luckz@gmail.com>
2013-04-29 11:57:48 +0100

    First Commit

---
+++ SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SimpleCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxL = new System.Windows.Forms.TextBox();
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.minusBtn = new System.Windows.Forms.Button();
+            this.multiBtn = new System.Windows.Forms.Button();
+            this.divBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter two numbers:";
+            // 
+            // textBoxL
+            // 
+            this.textBoxL.Location = new System.Drawing.Point(16, 30);
+            this.textBoxL.Name = "textBoxL";
+            this.textBoxL.Size = new System.Drawing.Size(122, 20);
+            this.textBoxL.TabIndex = 1;
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(150, 30);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(122, 20);
+            this.textBoxR.TabIndex = 2;
+            // 
+            // answerBox
+            // 
+            this.answerBox.Location = new System.Drawing.Point(81, 158);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.ReadOnly = true;
+            this.answerBox.Size = new System.Drawing.Size(129, 20);
+            this.answerBox.TabIndex = 3;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(16, 69);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(122, 23);
+            this.addBtn.TabIndex = 4;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Answer:";
+            // 
+            // minusBtn
+            // 
+            this.minusBtn.Location = new System.Drawing.Point(150, 69);
+            this.minusBtn.Name = "minusBtn";
+            this.minusBtn.Size = new System.Drawing.Size(122, 23);
+            this.minusBtn.TabIndex = 6;
+            this.minusBtn.Text = "Subtract";
+            this.minusBtn.UseVisualStyleBackColor = true;
+            this.minusBtn.Click += new System.EventHandler(this.minusBtn_Click);
+            // 
+            // multiBtn
+            // 
+            this.multiBtn.Location = new System.Drawing.Point(16, 99);
+            this.multiBtn.Name = "multiBtn";
+            this.multiBtn.Size = new System.Drawing.Size(122, 23);
+            this.multiBtn.TabIndex = 7;
+            this.multiBtn.Text = "Multiply";
+            this.multiBtn.UseVisualStyleBackColor = true;
+            this.multiBtn.Click += new System.EventHandler(this.multiBtn_Click);
+            // 
+            // divBtn
+            // 
+            this.divBtn.Location = new System.Drawing.Point(150, 99);
+            this.divBtn.Name = "divBtn";
+            this.divBtn.Size = new System.Drawing.Size(121, 23);
+            this.divBtn.TabIndex = 8;
+            this.divBtn.Text = "Divide";
+            this.divBtn.UseVisualStyleBackColor = true;
+            this.divBtn.Click += new System.EventHandler(this.divBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 193);
+            this.Controls.Add(this.divBtn);
+            this.Controls.Add(this.multiBtn);
+            this.Controls.Add(this.minusBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.textBoxR);
+            this.Controls.Add(this.textBoxL);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxL;
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button minusBtn;
+        private System.Windows.Forms.Button multiBtn;
+        private System.Windows.Forms.Button divBtn;
+    }
+}
+
---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            string strL = textBoxL.Text, strR = textBoxR.Text;
+            int intL = Convert.ToInt32(strL), intR = Convert.ToInt32(strR), intF = intL + intR;
+            answerBox.Text = Convert.ToString(intF);
+        }
+
+        private void minusBtn_Click(object sender, EventArgs e)
+        {
+            string strL = textBoxL.Text, strR = textBoxR.Text;
+            int intL = Convert.ToInt32(strL), intR = Convert.ToInt32(strR), intF = intL - intR;
+            answerBox.Text = Convert.ToString(intF);
+        }
+
+        private void multiBtn_Click(object sender, EventArgs e)
+        {
+            string strL = textBoxL.Text, strR = textBoxR.Text;
+            int intL = Convert.ToInt32(strL), intR = Convert.ToInt32(strR), intF = intL * intR;
+            answerBox.Text = Convert.ToString(intF);
+        }
+
+        private void divBtn_Click(object sender, EventArgs e)
+        {
+            string strL = textBoxL.Text, strR = textBoxR.Text;
+            int intL = Convert.ToInt32(strL), intR = Convert.ToInt32(strR);
+            if (intR == 0) { answerBox.Text = "Can't divide by zero!"; }
+            else { int intF = intL / intR; answerBox.Text = Convert.ToString(intF); }
+        }
+    }
+}
